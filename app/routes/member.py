5e8cc9390fd3e94670d790366adbcd8ae3e8457c
Jae Seong, Song <zzyzzy@gmail.com>
2024-02-05 10:20:30 +0900

    member route 작성

---
+++ app/routes/member.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+member_router = APIRouter()
+
+
+@member_router.get('/join')
+def join():
+    return {'msg': 'Hello, Join!'}
+
+
+@member_router.get('/login')
+def login():
+    return {'msg': 'Hello, Login!'}
+
+
+@member_router.get('/myinfo')
+def myinfo():
+    return {'msg': 'Hello, Myinfo!'}
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from app.routes.member import member_router
+
 app = FastAPI()
+
+# 외부 route 파일 불러오기
+app.include_router(member_router)
 
 
 @app.get("/")
@@ -11,3 +16,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == '_main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
