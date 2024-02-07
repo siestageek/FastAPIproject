989b075b3ace3ccb03489455e0047ca93486efce
Jae Seong, Song <zzyzzy@gmail.com>
2024-02-07 12:13:49 +0900

    게시판 리스트 - 백엔드

--- app/routes/board.py
+++ app/routes/board.py
@@ -3,6 +3,11 @@ from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from fastapi import status
+
+from app.schemas.board import NewBoard
+from app.services.board import BoardService
 
 board_router = APIRouter()
 
@@ -12,8 +17,9 @@ board_router.mount('/static', StaticFiles(directory='views/static'), name='stati
 
 @board_router.get('/list', response_class=HTMLResponse)
 def list(req: Request):
+    bdlist = BoardService.select_board()
     return templates.TemplateResponse(
-        'board/list.html', {'request': req})
+        'board/list.html', {'request': req, 'bdlist': bdlist})
 
 
 @board_router.get('/write', response_class=HTMLResponse)
@@ -21,6 +27,13 @@ def write(req: Request):
     return templates.TemplateResponse(
         'board/write.html', {'request': req})
 
+
+@board_router.post('/write')
+def writeok(bdto: NewBoard):
+    result = BoardService.insert_board(bdto)
+    res_url = '/error'
+    if result.rowcount > 0: res_url = '/board/list'
+    return RedirectResponse(res_url, status_code=status.HTTP_302_FOUND)
 
 
 @board_router.get('/view', response_class=HTMLResponse)
---
+++ app/services/board.py
@@ -0,0 +1,38 @@
+from sqlalchemy import insert, select
+
+from app.dbfactory import Session
+from app.models.board import Board
+
+
+class BoardService():
+    @staticmethod
+    def board_convert(bdto):
+        data = bdto.model_dump()
+        bd = Board(**data)
+        data = {'userid': bd.userid, 'title': bd.title,
+                'contents': bd.contents}
+        return data
+
+
+    @staticmethod
+    def insert_board(bdto):
+        data = BoardService.board_convert(bdto)
+        with Session() as sess:
+            stmt = insert(Board).values(data)
+            result = sess.execute(stmt)
+            sess.commit()
+
+        return result
+
+    @staticmethod
+    def select_board():
+        with Session() as sess:
+            stmt = select(Board.bno, Board.title, Board.userid,
+                      Board.regdate, Board.views)\
+            .order_by(Board.bno.desc())\
+            .offset(0).limit(25)
+            result = sess.execute(stmt)
+
+        return result
+
+
