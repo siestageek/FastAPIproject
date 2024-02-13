62dab8931c578f6c10b8ee59ee75e062ffb3c2f2
Jae Seong, Song <zzyzzy@gmail.com>
2024-02-13 09:42:32 +0900

    on_startup을 lifespan으로 변경

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -7,7 +9,16 @@ from app.dbfactory import db_startup
 from app.routes.board import board_router
 from app.routes.member import member_router
 
-app = FastAPI()
+
+# 서버시작시 디비 생성
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    db_startup()
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 # jinja2 설정
 templates = Jinja2Templates(directory='views/templates')
@@ -18,18 +29,12 @@ app.include_router(member_router)
 app.include_router(board_router, prefix='/board')
 
 
-# 서버시작시 디비 생성
-@app.on_event('startup')
-async def on_startup():
-    db_startup()
-
-
 @app.get("/", response_class=HTMLResponse)
 async def index(req: Request):
     return templates.TemplateResponse(
         'index.html', {'request': req})
 
 
-if __name__ == '_main__':
+if __name__ == '__main__':
     import uvicorn
     uvicorn.run('main:app', reload=True)
