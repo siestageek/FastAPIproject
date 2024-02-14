9695161086e00141ccbf19c607fec05425b26bb6
Jae Seong, Song <zzyzzy@gmail.com>
2024-02-14 13:44:56 +0900

    갤러리 1 - 프론트엔드

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
- # FASTAPI Project
+# FASTAPI Project
  
 * FastAPI
 * sqlAlchemy
 * pydantic
 * jinja2 template
 * pydantic_settings
+* python-multipart
+
+## nginx를 이용한 이미지 cdn 서버 구축
+* nginx 다운로드 및 압축해제 (c:/Java)
+* 이미지 저장 폴더(cdn) 생성 (C:/Java/nginx-1.25.3/html)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.staticfiles import StaticFiles
 
 from app.dbfactory import db_startup
 from app.routes.board import board_router
+from app.routes.gallery import gallery_router
 from app.routes.member import member_router
 
 
@@ -27,6 +28,7 @@ app.mount('/static', StaticFiles(directory='views/static'), name='static')
 # 외부 route 파일 불러오기
 app.include_router(member_router)
 app.include_router(board_router, prefix='/board')
+app.include_router(gallery_router, prefix='/gallery')
 
 
 @app.get("/", response_class=HTMLResponse)
